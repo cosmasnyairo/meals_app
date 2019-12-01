ba194ca6dff9bf7a31564731459d0ea0a3a216f7
Cosmas Nyairo <nyairocosmas@gmail.com>
2019-12-01 07:06:14 +0300

    Added Dummy Data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -4,9 +4,15 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-          children: <Widget>[
-            
-          ],      
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: <Widget>[
+        
+      ],
     );
   }
 }
---
+++ lib/dummy_data.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Mandazi',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Chai',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Chapati',
+    color: Colors.white,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Ugali & Beef',
+    color: Colors.white,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Mutura',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Nyama Choma',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Chips',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c8',
+    title: ' Sukuma Wiki',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Pilau',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Githeri',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c11',
+    title: ' Coleslaw Salad',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c12',
+    title: ' Mabuyu',
+    color: Colors.red,
+  ),
+];
