47e0ec7ea416ff233a6a7b44a78dcb7c34c8b0fc
Cosmas Nyairo <nyairocosmas@gmail.com>
2019-12-01 07:12:43 +0300

    Added Category Item

---
+++ lib/category_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem(
+    this.title,
+    this.color,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      
+    );
+  }
+}
